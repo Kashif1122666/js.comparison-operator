106f4a333068230eda7445c128736eacec975d6e
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-07-26 14:16:47 -0700

    Add files via upload
    js.comparison-operator

---
+++ js.comparison-operator/comparison-operator.js
@@ -0,0 +1,71 @@
+// CHAPTER-11 COMPARISON OPERATOR
+
+// equall ===, ==
+// ==
+// console.log(1 == 1);  // true
+// console.log(1 == '1'); //true
+// console.log(1 == 2); //false
+
+// ===
+// console.log(1 === 1); //true
+// console.log(1 === '1'); //false
+// console.log(1 === 2); //false
+
+// not(!)
+//it converts true to false, false to true
+// console.log(!true); // false
+// console.log(!false); // true
+
+// not equal (!==, !=)
+
+// (!=)
+// console.log(1 != 1);//false
+// console.log(1 != '1'); // false
+// console.log(1 != 2); // true
+
+
+// (!==)
+// console.log(1 !== 1);//false
+// console.log(1 !== '1'); // true
+// console.log(1 !== 2); // true
+
+// greater than >,
+// console.log(1 > 2); // false
+// console.log(2 > 1); // true
+// console.log(1 > 1); // false
+// console.log(1 > '1'); // false
+// console.log(1 > 0); // true
+
+// less than <
+// console.log(1 < 2); // true
+// console.log(2 < 1); // false
+// console.log(1 < 1); // false
+// console.log(1 < '1'); // false
+// console.log(1 < 0); // false
+// console.log(0 < -1); // false
+
+
+// greater than or equal to >=,
+// console.log(1 >= 1); // true
+// console.log(2 >= 3); // false
+// console.log(15 >= 15); // true
+// console.log(1 >= '1'); // true
+// console.log(1 >= 0); // true
+
+
+// less than or equal >=,
+// console.log(1 <= 2); // true
+// console.log(2 <= 1); // false
+// console.log(1 <= 1); // true
+// console.log(1 <= '1'); // true
+// console.log(1 <= 0); // false
+// console.log(0 <= -1); // false
+
+// or (||) if any of the given condition true so its true otherwise false 
+// console.log(1 === '1' || 1 > '1');//false
+// console.log(1 === '1' || 1 == '1');//true
+
+// and (&&) if all of the given conditions are true so its true otherwise false 
+// console.log(1 === '1' && 1 > '1');//false
+// console.log(1 === 1 && 1 == '1');//true
+
